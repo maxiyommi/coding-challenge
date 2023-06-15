4e2fe863e4545add6303b9bd4bd5f85f9f0fadc1
GonzaRJ <rodriguez45984@estudiantes.untref.edu.ar>
2023-06-15 00:24:25 -0300

    Corrección Aubone

--- ignacio_aubone/main.py
+++ ignacio_aubone/main.py
@@ -1,20 +1,20 @@
-import ejercicio_1
+from ejercicio_1 import funcion_1
 import ejercicio_2
 import ejercicio_3
 import ejercicio_4 
 
 # Ejercicio 1
-funcion_1()
+funcion_1()  ##1 forma
 
 # Ejercicio 2
-funcion_2()
+ejercicio_2.funcion_2()  ##otra forma
 
 # Ejercicio 3
 audio = 'ruido_rosa.wav'
-caracterizar_audio(audio_1)
+ejercicio_3.caracterizar_audio(audio)
 
 # Ejercicio 4
 audio1 = 'sine_sweep_recinto.wav'
 audio2 = 'filtro_inverso.wav'
-convolucion_audio(audio1, audio2)
+ejercicio_4.convolucion_audio(audio1, audio2)
 
